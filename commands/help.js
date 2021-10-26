f00c017a68386eed772e60b71743350b3e265980
rojbar <55609284+rojbar@users.noreply.github.com>
2021-10-25 19:07:43 -0500

    Se cambia de lugar el archivo de carga de comandos y se actualiza help

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Replies with help!'),
+		.setDescription('we should add a website link that is send to the user!'),
 	async execute(interaction) {
 		await interaction.reply('help!');
 	},
--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -1,6 +1,6 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('.././config.json');
+const { clientId, guildId, token } = require('../config.json');
 const fs = require('fs');
 
 const commands = [];
