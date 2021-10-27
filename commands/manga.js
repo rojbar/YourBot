758752c3bce86f9416e15f1cc812d40e3dfa5889
rojbar <55609284+rojbar@users.noreply.github.com>
2021-10-27 00:21:41 -0500

    Se añade la propiedad sources pero no se implementa la funcion que carga las sources

--- commands/manga.js
+++ commands/manga.js
@@ -7,6 +7,7 @@ manga.data.setDescription('It allows you to read manga!');
 
 manga.loadSubcommands();
 
+manga.sources = ["here we load all the manga sources in a collection where (name_source, source)"];
 
 module.exports = manga;
 
