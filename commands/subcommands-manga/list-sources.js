54889923783fe978d5f31db6113db11984927301
rojbar <55609284+rojbar@users.noreply.github.com>
2021-10-26 23:05:05 -0500

    Se implementa list-sources

--- commands/subcommands-manga/list-sources.js
+++ commands/subcommands-manga/list-sources.js
@@ -1,12 +1,15 @@
 const SubCommand = require('../../core/subcommand.js');
 const { sourceModel } = require('../../databases/manga/seeders/manga_manager.js');
-
+const enviroment = require('./helpers/enviroment.js');
+const {MessageEmbed} = require('discord.js');
 
 const list_sources = new SubCommand();
 list_sources.data.setName('list-sources');
 list_sources.data.setDescription('Shows the available sources for manga searches');
 
 list_sources.execute = async interaction =>{
+
+    await enviroment(interaction.user.id);
     
     const sources = await sourceModel.findAll();    
 
@@ -15,7 +18,7 @@ list_sources.execute = async interaction =>{
         reply.addField(`${element.source_id}. ${element.name}`, `${element.url} \n ${element.language}`, false);
     }); 
     
-    message.channel.send(reply);
+    interaction.reply({embeds: [reply]});
 
 }
 
