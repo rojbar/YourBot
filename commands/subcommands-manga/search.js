9a38a714cb87d9dd0e400cebbea42ff2ece28429
rojbar <55609284+rojbar@users.noreply.github.com>
2021-10-27 11:15:44 -0500

    Se implementa parte de search, cambiar el collector por un menu de opciones

--- commands/subcommands-manga/search.js
+++ commands/subcommands-manga/search.js
@@ -1,32 +1,40 @@
 const SubCommand = require('../../core/subcommand.js');
 const { sourceModel,enviromentModel, libraryModel, mangaModel} = require('../../databases/manga/seeders/manga_manager.js');
 const { MessageEmbed } = require('discord.js');
+const enviroment = require('./helpers/enviroment.js');
 
 
 const search = new SubCommand();
 search.data.setName('search');
 search.data.setDescription('searches a manga with the default source');
+read.data.addStringOption(      
+    option => 
+        option.setName('manga_id')
+          .setDescription('The name of the manga')
+          .setRequired(true)
+);
 
 search.execute = async interaction =>{
 
-    const enviroment = await enviromentModel.findByPk(message.author.id);
+    await enviroment(interaction.user.id);
+
+    const enviroment = await enviromentModel.findByPk(interaction.user.id);
     const default_source = await sourceModel.findByPk(enviroment.default_source);
    
 
-    if(args.sources.has(default_source.name))
+    if(interaction.client.commands.get(interaction.commandName).sources.has(default_source.name))
     {
-        const sourceScraper = args.sources.get(default_source.name);
+        const sourceScraper = interaction.client.commands.get(interaction.commandName).sources.get(default_source.name);
 
-        const busquedaFormateada = sourceScraper.formatSearch(args);
-        const resultados = await sourceScraper.search(sourceScraper.getSearchUrl().concat(busquedaFormateada));
+        const resultados = await sourceScraper.search(sourceScraper.getSearchUrl().concat(interaction.options.getString('manga_id')));
 
         const reply = new MessageEmbed().setTitle('Search Results:');
         resultados.forEach( function(element, index){
             reply.addField(`${index+1}. ${element.name} | Status: ${element.state}`, `${element.author} \n ${element.last_chapter}`, false);
         }); 
-        message.channel.send(reply);
+        interaction.reply(reply);
 
-        message.channel.awaitMessages( m => !m.author.bot,{max: 1}).then(
+        interaction.channel.awaitMessages( m => !m.author.bot,{max: 1}).then(
            async resultado => 
             {
                 let optionSelected =parseInt( resultado.first().content);
@@ -50,12 +58,12 @@ search.execute = async interaction =>{
                     await libraryModel.create(
                         {
                             manga_id: mangaResult[0].manga_id ,
-                            user_id: message.author.id,
+                            user_id: interaction.user.id,
                             last_chapter_read: 0,
-                        }).then( message.channel.send("Manga added to the library!"));
+                        }).then( interaction.reply("Manga added to the library!"));
                 }
                 else{
-                    message.channel.send('Operation canceled');
+                    interaction.reply('Operation canceled');
                 }
             }
         ).catch(error => console.log(error));
