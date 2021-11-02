d70f2bf0cafc635c0a154cc6f1686348b1c27e97
rojbar <55609284+rojbar@users.noreply.github.com>
2021-11-02 17:01:23 -0500

    Comando search listo

--- commands/subcommands-manga/search.js
+++ commands/subcommands-manga/search.js
@@ -1,6 +1,6 @@
 const SubCommand = require('../../core/subcommand.js');
 const { sourceModel,enviromentModel, libraryModel, mangaModel} = require('../../databases/manga/seeders/manga_manager.js');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
 const env = require('./helpers/enviroment.js');
 
 
@@ -15,60 +15,98 @@ search.data.addStringOption(
 );
 
 search.execute = async interaction =>{
-
+    
+    await interaction.deferReply();
+    
     await env(interaction.user.id);
 
     const enviroment = await enviromentModel.findByPk(interaction.user.id);
     const default_source = await sourceModel.findByPk(enviroment.default_source);
    
 
-    if(interaction.client.commands.get(interaction.commandName).sources.has(default_source.name))
-    {
-        const sourceScraper = interaction.client.commands.get(interaction.commandName).sources.get(default_source.name);
-
-        const resultados = await sourceScraper.search(sourceScraper.getSearchUrl().concat(interaction.options.getString('name')));
-
-        const reply = new MessageEmbed().setTitle('Search Results:');
-        resultados.forEach( function(element, index){
-            reply.addField(`${index+1}. ${element.name} | Status: ${element.state}`, `${element.author} \n ${element.last_chapter}`, false);
-        }); 
-        interaction.reply(reply);
-
-        interaction.channel.awaitMessages( m => !m.author.bot,{max: 1}).then(
-           async resultado => 
-            {
-                let optionSelected =parseInt( resultado.first().content);
-                if(optionSelected > 0)
-                {
-                    const mangaResult = await mangaModel.findOrCreate( 
-                        {
-                            where: {
-                                name: resultados[optionSelected-1].name,
-                                source_id: default_source.source_id,
-                                chapter_url: resultados[optionSelected-1].chapterURL,
-                            },
-                            defaults:{
-                                name: resultados[optionSelected-1].name,
-                                source_id: default_source.source_id ,
-                                chapter_url: resultados[optionSelected-1].chapterURL,
-                                },
-                            fields: ['name','source_id','chapter_url'],
-                        }
-                    );
-                    await libraryModel.create(
-                        {
-                            manga_id: mangaResult[0].manga_id ,
-                            user_id: interaction.user.id,
-                            last_chapter_read: 0,
-                        }).then( interaction.reply("Manga added to the library!"));
-                }
-                else{
-                    interaction.reply('Operation canceled');
-                }
-            }
-        ).catch(error => console.log(error));
+    if(!interaction.client.commands.get(interaction.commandName).sources.has(default_source.name))
+    { 
+        interaction.reply('There was an error');
+        return;
     }
+    
+    const sourceScraper = interaction.client.commands.get(interaction.commandName).sources.get(default_source.name);
 
+    const resultados = await sourceScraper.search(sourceScraper.getSearchUrl().concat(interaction.options.getString('name')));
+
+    const reply = new MessageEmbed().setTitle('Search Results:').setDescription('Select a manga to add');
+
+    const menuOptions = [];
+    resultados.forEach( function(element, index){
+        menuOptions.push({
+            label: `${element.name}`,
+            description: ` `,
+            value: `${index}`
+        });
+    });
+
+    
+    const row = new MessageActionRow() 
+			.addComponents(
+                new MessageSelectMenu().setCustomId('selectManga')
+                                       .setMinValues(1)
+                                       .setMaxValues(1)
+                                       .setPlaceholder('select the manga to read!')
+                                       .addOptions(menuOptions)
+			);
+
+    await interaction.editReply({content:'Your results:',embeds: [reply], components:[row]});
+
+    const filter = i => {
+        i.deferUpdate();
+        return i.user.id === interaction.user.id;
+    };
+    
+    await interaction.channel.awaitMessageComponent({ filter, componentType: 'SELECT_MENU', time: 60000 })
+        .then( async (selectMenuInteraction) =>{
+                
+                mangaSelected = resultados[selectMenuInteraction.values[0]];
+
+                const mangaResult = await mangaModel.findOrCreate( 
+                    {
+                        where: {
+                            name: mangaSelected.name,
+                            source_id: default_source.source_id,
+                            chapter_url: mangaSelected.chapterURL,
+                        },
+                        defaults:{
+                            name: mangaSelected.name,
+                            source_id: default_source.source_id ,
+                            chapter_url: mangaSelected.chapterURL,
+                            },
+                        fields: ['name','source_id','chapter_url'],
+                    }
+                ).catch( (error) => {
+                    console.error(error);
+                });
+
+                await libraryModel.create(
+                {
+                    manga_id: mangaResult[0].manga_id ,
+                    user_id: selectMenuInteraction.user.id,
+                    last_chapter_read: 0,
+                }).then( async (resp) =>{
+
+                    await selectMenuInteraction.followUp("Manga added to the library!");
+                })
+                .catch( async (error) => {
+                    console.error(error);
+                    await selectMenuInteraction.followUp("Something went wrong");
+                });
+            }
+        )
+        .catch( async (error) => {
+                console.error(error);
+                await interaction.followUp('Not manga selected');
+             }
+        );
+    
+    await interaction.deleteReply();
 }
 
 module.exports = search;
